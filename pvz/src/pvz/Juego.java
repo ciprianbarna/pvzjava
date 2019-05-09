d3217d4a686c7c01dc4738cc5d3e6d77ec1b84b9
LauGS <laurardark@gmail.com>
2019-05-09 16:54:37 +0200

    Importadas Random y Scanner en Juego

--- pvz/src/pvz/Juego.java
+++ pvz/src/pvz/Juego.java
@@ -7,14 +7,14 @@ public class Juego {
     public static void main (String[] args){
 
         static final int solesInicial = 50; //soles al inicio de partida
-        private turno = 0; //turno actual
+        private int turno = 0; //turno actual
 
 
         Girasol g = new Girasol();
         LanzaGuisantes l = new LanzaGuisantes();
         Zombie z = new Zombie();
 
-        g.imprimir();
+        g.
         l.imprimir();
         z.imprimir();
 
